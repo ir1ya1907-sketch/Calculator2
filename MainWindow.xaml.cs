d63ea5c7fc38444c9dfb2f5c422677a8beda39f9
Michael <mishamu>
2026-05-04 14:42:18 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CalculatorApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9,-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void OnOperateClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(InputA.Text, out double a) &&
+                double.TryParse(InputB.Text, out double b))
+            {
+                string op = (sender as FrameworkElement).Tag.ToString();
+                double res = 0;
+
+                switch (op)
+                {
+                    case "+": res = a + b; break;
+                    case "-": res = a - b; break;
+                    case "*": res = a * b; break;
+                    case "/":
+                        if (b == 0) { MessageBox.Show("Деление на 0!"); return; }
+                        res = a / b; break;
+                }
+
+                ResultLabel.Text = $"Результат: {res}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные числа");
+            }
+        }
+    }
+}
